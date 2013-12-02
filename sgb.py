927f883b2a9062c7f4c5634a459d34f040b1ae47
Alexander Serebryakov <alex.serebr@gmail.com>
2013-12-02 09:52:15 +0200

    This commit adds input parameters handling

--- sgb.py
+++ sgb.py
@@ -7,8 +7,16 @@ VERSION = "0.01"
 
 def main(argv):
     print('{0} v{1}'.format(NAME, VERSION))
+
+    if len(argv) != 2:
+        print('Usage: sgb <input> <output>')
+        return 0
+
+    print('Opening files...')
     graph_bldr = GraphBuilder(argv[0], argv[1])
+    print('Generating output...')
     graph_bldr.Generate();
+    print('Graph is saved in {0}'.format(argv[1]))
 
     return 0
 
