b3cca39c034ecbeff3aa2808d6dcfe52bbf8ced2
Alexander Serebryakov <alex.serebr@gmail.com>
2014-02-18 22:26:13 +0200

    Added argument parsing

--- sgb.py
+++ sgb.py
@@ -1,25 +1,28 @@
 import sys
+import argparse
 from  graph_builder import GraphBuilder
 
 NAME = "stack-graph-builder"
 VERSION = "0.02"
 
 
-def main(argv):
-    print('{0} v{1}'.format(NAME, VERSION))
-
-    if len(argv) != 2:
-        print('Usage: sgb <input> <output>')
-        return 0
-
+def main(infile, outfile):
     print('Opening files...')
-    graph_bldr = GraphBuilder(argv[0], argv[1])
+    graph_bldr = GraphBuilder(infile, outfile)
     print('Generating output...')
-    graph_bldr.generate();
-    print('Graph is saved in {0}'.format(argv[1]))
+    graph_bldr.generate()
+    print('Graph is saved in {0}'.format(outfile))
 
     return 0
 
 
 if __name__ == "__main__":
-    sys.exit(main(sys.argv[1:]))
+    parser = argparse.ArgumentParser()
+    parser.add_argument("input_file", help = "input file name")
+    parser.add_argument("output_file", help = "output file name")
+
+    args = parser.parse_args()
+
+    print('{0} v{1}'.format(NAME, VERSION))
+
+    sys.exit(main(args.input_file, args.output_file))
