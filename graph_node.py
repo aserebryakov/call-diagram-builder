ab5f1a7d75204da71facaac57839d8c0e25815c1
Alexander Serebryakov <alex.serebr@gmail.com>
2014-02-18 23:59:22 +0200

    Fixed coding style issues

--- graph_node.py
+++ graph_node.py
@@ -15,7 +15,7 @@ class GraphNode:
 
     def calculate_indent(self, line):
         match = re.match(r'\s+', line)
-        indent = 0 
+        indent = 0
 
         if match is not None:
             indent = len(re.findall(r'\s', match.string))//self.indentlengh
@@ -24,9 +24,9 @@ class GraphNode:
 
     def __parse(self, line):
         self.__label  = re.sub(r'\s*', '', line, 1)
-        self.__node_id = 'node_' + hashlib.md5(self.__label).hexdigest()
+        self.__node_id = 'node_{0}'.format(hashlib.md5(self.__label).hexdigest())
         self.__indent = self.calculate_indent(line)
-         
+
     @property
     def node_id(self):
         return self.__node_id
@@ -37,5 +37,5 @@ class GraphNode:
 
     def get_description(self):
         description = self.__node_id + '[' + self.stylestring + ', label="' + self.__label + '"];\n'
-        return description 
+        return description
 
