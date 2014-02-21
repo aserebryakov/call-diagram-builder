3afe23ae3dbe2d99449a2eb67cb16a16f17f8d60
Alexander Serebryakov <alex.serebr@gmail.com>
2014-02-21 19:45:06 +0200

    Auto-rendering feature was removed until it will be fixed

--- README.md
+++ README.md
@@ -31,7 +31,6 @@ positional arguments:
 
 optional arguments:
   -h, --help    show this help message and exit
-  -r, --render  call DOT to render graph after generation
 
 ```
 
--- cdb.py
+++ cdb.py
@@ -7,22 +7,6 @@ from  graph_builder import GraphBuilder
 NAME = 'call-diagram-builder'
 VERSION = '0.02'
 
-def render_graph(output_file):
-    result = 1
-
-    try:
-        image_name = re.sub(r'\..*', '.png', output_file)
-        args = ['dot', output_file, '-Tpng', '-o', image_name]
-        result = subprocess.call(args)
-
-        if(result == 0):
-            print('Graph is rendered to {0}'.format(image_name))
-
-    except:
-        print('ERROR: Cannot run DOT. Please check your PATH')
-
-    return result
-
 
 def main(args):
     result = 0
@@ -33,9 +17,6 @@ def main(args):
     graph_bldr.generate()
     print('Graph is saved in {0}'.format(args.output_file))
 
-    if(args.render):
-        result = render_graph(args.output_file)
-
     return result
 
 
@@ -43,8 +24,6 @@ if __name__ == "__main__":
     parser = argparse.ArgumentParser()
     parser.add_argument('input_file', help = 'input file name')
     parser.add_argument('output_file', help = 'output file name')
-    parser.add_argument('-r', '--render', help = 'call DOT to render graph after generation', \
-                        action = 'store_true')
 
     args = parser.parse_args()
 
